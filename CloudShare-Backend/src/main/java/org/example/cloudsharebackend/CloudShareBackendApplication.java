fac506325ec0887cd0ab3cb69d3b47e4312f616c
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-08-20 12:33:03 +0530

    Project setup

---
+++ CloudShare-Backend/src/main/java/org/example/cloudsharebackend/CloudShareBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.cloudsharebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudShareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudShareBackendApplication.class, args);
+    }
+
+}
---
+++ CloudShare-Backend/src/test/java/org/example/cloudsharebackend/CloudShareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloudsharebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudShareBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
